61841c338978de9ae1f8392653c0c22394dfc9d9
Brian Illes <brianrilles@gmail.com>
2019-07-08 13:50:14 -0700

    MVP complete

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,42 +15,61 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.array = [None] * capacity
 
 # '''
 # Fill this in.
 # Research and implement the djb2 hash function
+# [keys] -> | hash function | -> [hashes]
+# [John Smith] -> HF -> [02]
 # '''
-def hash(string, max):
-    pass
 
+
+def hash(string, max):
+    hash = 5381
+    for i in string:
+        hash = ((hash << 5) + hash) + max
+    hash = max % hash - 1
+    return hash
 
 # '''
 # Fill this in.
 
 # If you are overwriting a value with a different key, print a warning.
 # '''
-def hash_table_insert(hash_table, key, value):
-    pass
 
+
+def hash_table_insert(hash_table, key, value):
+    hash_key = hash(key, len(hash_table.array))
+    if hash_table.array[hash_key] is not None:
+        print("WARNING: overwriting a value with a different key")
+    hash_table.array[hash_key] = value
 
 # '''
 # Fill this in.
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
+
+
 def hash_table_remove(hash_table, key):
-    pass
-
-
+    key = hash(key, len(hash_table.array))
+    if hash_table.array[key] is None:
+        print("WARNING: attempting to remove a value that isn't there")
+    hash_table.array[key] = None
 # '''
 # Fill this in.
 
 # Should return None if the key is not found.
 # '''
+
+
 def hash_table_retrieve(hash_table, key):
-    pass
+    if hash_table.array[hash(key, len(hash_table.array))] is None:
+        return None
+    else:
+        return hash_table[key]
 
 
 def Testing():
